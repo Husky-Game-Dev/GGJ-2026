7c203ebde7cbebd755e2bc2eb1f302529a8d7b50
Caeden117 <caeden.s@outlook.com>
2026-01-31 00:33:57 -0800

    Import visual novel system from Phil Buster

---
+++ scripts/visual_novel/dialog_box/dialog_box.gd
@@ -0,0 +1,138 @@
+class_name DialogBox
+extends Control
+
+# DialogBox code is based from its implementation in Bubble Sorter, improved for use in Phil Buster.
+# TODO:
+# - Expose animation for dialog box opening and closing
+# - Check if cursor is over other UI elements before emitting _user_interacted signal
+# - Rewrite display_string to use variadic arguments once Godot 4.5 is released
+
+##
+## Signals
+##
+
+# Internal signal to indicate user interaction, which can be used to skip dialog.
+signal _user_interacted()
+
+##
+## Variables
+##
+
+## Icon to display inside the dialog box when an entry is finished.
+@export
+var finished_icon: Texture2D
+
+## The speed of the dialog box, in characters per second
+@export
+var talking_speed: float = 25.0
+
+## RichTextLabel to display dialog.
+@onready
+var _dialog_label: RichTextLabel = %dialog_label
+
+## RichTextLabel to display the character name.
+@onready
+var _character_name_label: RichTextLabel = %character_name_label
+
+## Container for the character name label, which will be hidden if the name is empty.
+@onready
+var _character_name_container: Control = %character_name_container
+
+## AudioStreamPlayer which will play with every character
+@onready
+var _sfx_audio_player: AudioStreamPlayer = %sfx_player
+
+## Timer to control the delay between characters
+@onready
+var _character_delay_timer: Timer = %character_delay_timer
+
+## Previous dialog entry, held in case we need to extend it.
+var _previous_dialog: String = ""
+
+##
+## Methods
+##
+
+## Sets the character name label to the given name.
+func display_character_name(character_name: String) -> void:
+	# Sets the character name label to the given name and makes it visible
+	_character_name_label.text = character_name
+	_character_name_container.visible = !character_name.is_empty()
+
+## Displays a list of dialog entries, waiting for user interaction between each entry.
+# Variadic version for 4.5: func display_string(...entries: Array) -> void:
+func display_string(entries: Array[String]) -> void:
+	for entry: String in entries:
+		await _display_string(entry)
+		await _user_interacted
+
+## Extends a dialog entry with new text.
+func extend_string(entries: Array[String], concat_text: String = " ") -> void:
+	for entry: String in entries:
+		var extended_dialog: String = "%s%s%s" % [_previous_dialog, concat_text, entry]
+
+		await _display_string(extended_dialog, _previous_dialog.length() + concat_text.length())
+		await _user_interacted
+
+## Controls whether dialog text is centered or aligned to the top-left.
+# Narration text should be centered, while character dialog should be aligned to the top-left.
+func set_dialog_centered(centered: bool) -> void:
+	if centered:
+		_dialog_label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+		_dialog_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	else:
+		_dialog_label.vertical_alignment = VERTICAL_ALIGNMENT_TOP
+		_dialog_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
+
+## Toggles the visibility of the dialog box.
+func set_dialog_visibility(dialog_visibility: bool) -> void:
+	if dialog_visibility and not self.visible:
+
+		# TODO: This is useless code that just awaits something so calling members do not receive a warning.
+		# Replace with actual animation code later.
+		_character_delay_timer.wait_time = 0.1
+		_character_delay_timer.start(0)
+		await _character_delay_timer.timeout
+
+		self.visible = true
+	elif not dialog_visibility and self.visible:
+		self.visible = false
+
+# Initially hides the dialog box, skipping any animation.
+func _ready() -> void:
+	self.visible = false
+
+# Emit our interaction signal if any keyboard or mouse press is detected
+func _input(event: InputEvent) -> void:
+	if (event is InputEventMouseButton or event is InputEventKey) and event.is_pressed():
+		_dialog_label.visible_ratio = 1
+		_user_interacted.emit()
+
+# Animates one line of text, instantly finishing upon any user interaction
+func _display_string(dialog: String, starting_position: int = 0) -> void:
+	# Clears dialog label, including the finished icon if it was previously displayed
+	_dialog_label.clear()
+
+	var characterDelay: float = 1 / talking_speed 
+	_character_delay_timer.wait_time = characterDelay
+	
+	_previous_dialog = dialog
+	_dialog_label.text = dialog
+	_dialog_label.visible_characters = starting_position
+	
+	# We check against visible_ratio because it can be set to 1 by user interaction
+	while _dialog_label.visible_ratio < 1:
+		# Show the next character and await the character delay timer
+		_dialog_label.visible_characters += 1
+		
+		if is_instance_valid(_sfx_audio_player):
+			_sfx_audio_player.play()
+		
+		_character_delay_timer.start()
+		await _character_delay_timer.timeout
+	
+	if is_instance_valid(finished_icon):
+		_dialog_label.append_text(" ")
+		_dialog_label.add_image(finished_icon, 40)
+	
+	_dialog_label.visible_characters = -1
---
+++ scripts/visual_novel/visual_novel_scenario.gd
@@ -0,0 +1,54 @@
+class_name VisualNovelScenario
+extends Control
+
+# TODO:
+# - Character resource which holds name, portraits, emotions, and other metadata
+# - Portrait display and management
+# - Scene background display and management
+# - Music and sound effect management
+# - Choices and branching dialog(??)
+# - Test extending scenarios off of base class
+# - Perhaps consider a reading log system so players can review past dialog
+# - Switch to using variadic arguments for dialog once Godot 4.5 is released
+
+###
+### Variables
+###
+
+@onready
+var _dialog_box: DialogBox = %dialog_box
+
+###
+### Methods
+###
+
+func show_dialog_box() -> void:
+	await _dialog_box.set_dialog_visibility(true)
+
+func hide_dialog_box() -> void:
+	await _dialog_box.set_dialog_visibility(false)
+
+# GDScript does not and will not support method overloading. This is not ideal.
+func narrate(dialog: Array[String]) -> void:
+	# Ensure all lines are italicized
+	for i: int in range(dialog.size()):
+		var line: String = dialog[i]
+	
+		if not line.begins_with("[i]"):
+			line = "[i]%s[/i]" % line
+			dialog[i] = line
+
+	_dialog_box.display_character_name("")
+	_dialog_box.set_dialog_centered(true)
+	await _dialog_box.set_dialog_visibility(true)
+	await _dialog_box.display_string(dialog)
+
+func speak(character_name: String, dialog: Array[String]) -> void:
+	_dialog_box.display_character_name(character_name)
+	_dialog_box.set_dialog_centered(false)
+	await _dialog_box.set_dialog_visibility(true)
+	await _dialog_box.display_string(dialog)
+
+func extend(dialog: Array[String]) -> void:
+	await _dialog_box.set_dialog_visibility(true)
+	await _dialog_box.extend_string(dialog)
