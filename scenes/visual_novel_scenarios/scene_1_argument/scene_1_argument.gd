ff26743e4c7e08f9107c5fb3e929f288ea4535e0
Caeden117 <caeden.s@outlook.com>
2026-01-31 17:23:50 -0800

    Fix character sprite loading

--- scenes/visual_novel_scenarios/scene_1_argument/scene_1_argument.gd
+++ scenes/visual_novel_scenarios/scene_1_argument/scene_1_argument.gd
@@ -22,18 +22,19 @@ func run_scenario() -> void:
 	await character_speak(waiter, [ "No! Lunch is in 30. We need to feed these guests!" ])
 	await character_speak(chef, ["Come on, it's not a crime to catch your breath."])
 	await character_speak(waiter, ["ACTUALLY, rule 5 section 1 states that ‘no kitchen equipment is allowed a break during working hours.'"])
-	await character_speak(waiter, ["But rule 5 section 4 says that ‘a work week consists of 20 hours per day, 7 days a week!’"])
+	await character_speak(chef, ["But rule 5 section 4 says that ‘a work week consists of 20 hours per day, 7 days a week!’"])
 	await character_speak(chef, ["Hey newcomer, can't you see how crazy this is?!"])
 	await character_speak(player, ["20 hours per day is a bit unreasonable…"])
 	await character_speak(chef, ["SEE?!?"])
 	await character_speak(waiter, ["Fine, whatever, let's see what everyone else has to say about it!"])
 	
+	await narrate([ "The Waiter stomps off, clearly angry with the argument...", "After things cool down, The Chef turns towards you." ])
 	# Chef Dialogue
 	
 	await character_speak(chef, ["Everyone around here is so stuck up these days."])
 	await character_speak(chef, ["It’s not like this food goes anywhere, we've stopped getting visitors AGES ago."])
 	await character_speak(player, ["Then why are you even here? Can’t you just leave this place?"])
-	await character_speak(chef, ["I wish… It's all I have ever known. I can’t just abandon everyone else."])
-	await character_speak(chef, ["I say… If you find a way to convince everyone that this place is laughable then I will be forever in your debt. What do you say?"])
+	await character_speak(chef, ["I wish… It's all I have ever known. I can’t just abandon everyone else. 'The show must go on', as they say."])
+	await character_speak(chef, ["Hmm... If you find a way to convince everyone that this place is laughable then I will be forever in your debt. What do you say?"])
 	await character_speak(player, ["I’ll give it a shot!"])
 	await character_speak(chef, ["Everyone else is in the other room. Just for practice though, why don't you try to convice me that rule 5 is absurd?"])
