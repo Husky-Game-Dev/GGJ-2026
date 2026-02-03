b4a6c7158948494d649b5bf43bed2e9ac7eaf63f
Kaiser Lee <kaiserkaiserlee@gmail.com>
2026-02-03 14:22:03 -0800

    added tooltip for the puzzle pieces
    when you hover your mouse over the operator chip puzzle piece, a tool tip explaining the operator appears

--- scripts/ui/puzzle_piece_view.gd
+++ scripts/ui/puzzle_piece_view.gd
@@ -22,16 +22,40 @@ func _ready() -> void:
 	match model.operation:
 		PuzzlePieceModel.Operation.AND:
 			operation_label.text = "& "
+			tooltip_text = "AND operator \n
+				sets bit to 1 if both bits are 1, set bit to 0 otherwise. \n
+				1001 & 1010 \n
+				1000"
 		PuzzlePieceModel.Operation.OR:
 			operation_label.text = "| "
+			tooltip_text = "OR operator\n
+				sets bit to 1 if one of the bits are 1, set bit to 0 otherwise. \n
+				1001 | 1010 \n
+				1011"
 		PuzzlePieceModel.Operation.XOR:
 			operation_label.text = "^ "
+			tooltip_text = "XOR operator\n
+				sets bit to 1 if both bits are different, set bit to 0 otherwise. \n
+				1001 & 1010 \n
+				0011"
 		PuzzlePieceModel.Operation.LEFT_SHIFT:
 			operation_label.text = "<<"
+			tooltip_text = "L Shift operator\n
+				move existing numbers to the left, 0 takes place of bits that was not in original number \n
+				1001 << 2 \n
+				0100"
 		PuzzlePieceModel.Operation.RIGHT_SHIFT:
 			operation_label.text = ">>"
+			tooltip_text = "R Shift operator\n
+				move existing numbers to the right, 0 takes place of bits that was not in original number \n
+				1001 >> 2 \n
+				0010"
 		PuzzlePieceModel.Operation.NOT:
 			operation_label.text = "~ "
+			tooltip_text = "NOT operator\n
+				invert all bits, change 1 to 0 and 0 to 1 \n
+				1001 ~ \n
+				0110"
 	match model.operation:
 		PuzzlePieceModel.Operation.AND, PuzzlePieceModel.Operation.OR, PuzzlePieceModel.Operation.XOR:
 			value_label.text = String.num_uint64(model.bitmask, 2).lpad(PuzzlePieceModel.MAX_BITS, "0")
